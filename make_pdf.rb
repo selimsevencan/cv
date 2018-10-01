dc3de92c2777bf881c81381b112b1bbace0ec835
Eralp Karaduman <eralpkaraduman@gmail.com>
2018-10-01 16:32:01 +0300

    Fixed typo

--- make_pdf.rb
+++ make_pdf.rb
@@ -2,5 +2,5 @@ require 'pdfkit'
 
 puts "Generating PDF..."
 kit = PDFKit.new(File.new('./_site/index.html'), :page_size => 'Letter')
-kit.to_file('./_site/cv.pdf')
+file = kit.to_file('./_site/cv.pdf')
 puts "Generated PDF is here: #{file.path}"
