e704bf932ba67833d84abf1b5ab7d086f855e34b
Eralp Karaduman <eralpkaraduman@gmail.com>
2018-10-01 16:26:07 +0300

    Prints pdf path

--- make_pdf.rb
+++ make_pdf.rb
@@ -1,5 +1,6 @@
 require 'pdfkit'
 
+puts "Generating PDF..."
 kit = PDFKit.new(File.new('./_site/index.html'), :page_size => 'Letter')
 kit.to_file('./_site/cv.pdf')
-
+puts "Generated PDF is here: #{file.path}"
