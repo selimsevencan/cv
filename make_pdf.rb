f682816063b8252d9b58d45fe5410e905809dcb7
Eralp Karaduman <eralpkaraduman@gmail.com>
2018-10-02 17:26:27 +0300

    Attempting to fix pdf generationin

--- make_pdf.rb
+++ make_pdf.rb
@@ -1,5 +1,13 @@
 require 'pdfkit'
 
+PDFKit.configure do |config|
+	config.default_options = {
+		:debug_javascript => true,
+		:javascript_delay => "3000"
+	}
+	config.verbose = true
+end
+
 puts "Generating PDF..."
 kit = PDFKit.new(File.new('./_site/index.html'), :page_size => 'Letter')
 file = kit.to_file('./_site/cv.pdf')
